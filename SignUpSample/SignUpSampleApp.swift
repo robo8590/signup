ca052e2e81b1f71680a1cf867f4c0277909f932f
Nam Tran <robo8590@gmail.com>
2021-12-05 12:59:13 -0800

    B3: Create Xcode project and add script to scan code using swiftlint

---
+++ SignUpSample/SignUpSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpSampleApp.swift
+//  SignUpSample
+//
+//  Created by Nam Tran on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SignUpSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
