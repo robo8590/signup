f35a57685c725b2ed832689ee22be2b112aaaec3
Nam Tran <robo8590@gmail.com>
2021-12-06 10:46:26 -0800

    B7: Create RoundedTextFieldStyle

---
+++ SignUpSample/Resource/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  SignUpSample
+//
+//  Created by Nam Tran on 12/6/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkGray = Color("DarkGray")
+    static let lightGray = Color("LightGray")
+}
---
+++ SignUpSample/View/Controls/RoundedTextFieldStyle.swift
@@ -0,0 +1,54 @@
+//
+//  RoundedTextFieldStyle.swift
+//  SignUpSample
+//
+//  Created by Nam Tran on 12/6/21.
+//
+
+import SwiftUI
+
+struct RoundedTextFieldStyle: TextFieldStyle {
+    // swiftlint:disable:next identifier_name
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.lightGray, lineWidth: 2)
+            )
+    }
+}
+
+#if DEBUG
+struct RoundedTextFieldStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ZStack(alignment: .leading) {
+                Text("First Name (Optional)")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.darkGray)
+                    .padding()
+                TextField("", text: .constant(""))
+                    .textFieldStyle(RoundedTextFieldStyle())
+            }
+            .preferredColorScheme(.light)
+            .padding()
+            .previewLayout(.fixed(width: 400, height: 100))
+            .previewDisplayName("Light Mode")
+
+            ZStack(alignment: .leading) {
+                Text("First Name (Optional)")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.darkGray)
+                    .padding()
+                TextField("", text: .constant(""))
+                    .textFieldStyle(RoundedTextFieldStyle())
+            }
+            .preferredColorScheme(.dark)
+            .padding()
+            .previewLayout(.fixed(width: 400, height: 100))
+            .previewDisplayName("Dark Mode")
+        }
+    }
+}
+#endif
