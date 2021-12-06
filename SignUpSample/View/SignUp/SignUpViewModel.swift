2c8b66de50076a54a10c1ae0b976b740dd6aeb0b
Nam Tran <robo8590@gmail.com>
2021-12-05 21:36:03 -0800

    B7: Implement view model for US1-S1 - Scenario 1: The first name is optional

--- SignUpSample/View/SignUp/SignUpViewModel.swift
+++ SignUpSample/View/SignUp/SignUpViewModel.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+// MARK: - Define the steps
+/// The step of sign up process
+enum SignUpStep {
+    case enteringFirstName
+    case enteringEmail
+}
+
+// MARK: - Define the states
+/// The view model of sign up view
+class SignUpViewModel: ObservableObject {
+    /// The current step of sign up process
+    @Published var currentStep: SignUpStep = .enteringFirstName
+    /// The state of the next button
+    @Published var isNextButtonEnabled = true
+    /// The first name of the user
+    @Published var firstName: String = ""
+    /// The email of the user
+    @Published var email: String = ""
+}
+
+// MARK: - Input Validation
+extension SignUpViewModel {
+    /// Checks the current input is valid or not
+    var isCurrentInputValid: Bool {
+        switch currentStep {
+        case .enteringFirstName:
+            return isFirstNameValid
+        case .enteringEmail:
+            return isEmailValid
+        }
+    }
+
+    /// Checks the first name is valid
+    var isFirstNameValid: Bool {
+        guard !firstName.isEmpty else {
+            return true
+        }
+        return true
+    }
+
+    /// Checks the email is valid or not
+    var isEmailValid: Bool {
+        guard !email.isEmpty else {
+            return false
+        }
+        return true
+    }
+}
+
+// MARK: - Actions
+extension SignUpViewModel {
+    /// Go to the next step of sign up process
+    func next() {
+        guard isNextButtonEnabled else {
+            return
+        }
+        switch currentStep {
+        case .enteringFirstName:
+            currentStep = .enteringEmail
+        case .enteringEmail:
+            break
+        }
+        isNextButtonEnabled = isCurrentInputValid
+    }
+}
--- SignUpSampleTests/US1SignUp/US1S1FirstNameIsOptinalTests.swift
+++ SignUpSampleTests/US1SignUp/US1S1FirstNameIsOptinalTests.swift
@@ -4,17 +4,42 @@
 //
 //  Created by Nam Tran on 12/5/21.
 //
+//
+//  US1 - User story 1 : Sign Up
+//
+//  As a guest user,
+//  I want to create my account
+//  so that I can login to the system.
+//
+//  Entering the first name
+//
+//  US1-S1 - Scenario 1: The first name is optional.
+//
+//  Given I started the sign up wizard
+//  and I tapped to the first name text field
+//  and the keyboard was presented
+//  when I don't want to enter the first name
+//  and I tap to the next button
+//  then the email text field should be showed.
+//
 
 import XCTest
+@testable import SignUpSample
 
 class US1S1FirstNameIsOptinalTests: XCTestCase {
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testViewModel() {
+        let viewModel = SignUpViewModel()
 
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Given
+        XCTAssertEqual(viewModel.currentStep, .enteringFirstName)
+        XCTAssertEqual(viewModel.firstName, "")
+        XCTAssertEqual(viewModel.isNextButtonEnabled, true)
+
+        // When
+        viewModel.next()
+
+        // Then
+        XCTAssertEqual(viewModel.currentStep, .enteringEmail)
+        XCTAssertEqual(viewModel.isNextButtonEnabled, false)
     }
 }
