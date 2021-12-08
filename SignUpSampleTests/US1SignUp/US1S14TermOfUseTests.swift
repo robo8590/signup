aa7c1cf220a0d80d95b8f55597648c289995acd3
Nam Tran <robo8590@gmail.com>
2021-12-08 13:25:42 -0800

    B11: Test and code US1-S14 - Scenario 14: Term of use

--- SignUpSampleTests/US1SignUp/US1S14TermOfUseTests.swift
+++ SignUpSampleTests/US1SignUp/US1S14TermOfUseTests.swift
@@ -4,17 +4,41 @@
 //
 //  Created by Nam Tran on 12/5/21.
 //
+//  US1 - User story 1 : Sign Up
+//
+//  As a guest user,
+//  I want to create my account
+//  so that I can login to the system.
+//
+//  Viewing term of use
+//
+//  US1-S14 - Scenario 14: Term of use.
+//
+//  Given I started the sign up wizard
+//  when I go to the last step
+//  then I am able to view the term of use
+//  and the submit button is enable.
+//
 
 import XCTest
+@testable import SignUpSample
 
 class US1S14TermOfUseTests: XCTestCase {
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testViewModel() {
+        let viewModel = SignUpViewModel()
 
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Given
+        viewModel.firstName = "Nam"
+        viewModel.email = "robo8590@gmail.com"
+        viewModel.password = "Nam12!"
+        viewModel.website = "http://www.linkedin.com/in/robo8590"
+        viewModel.currentStep = .enteringWebsite
+
+        // When
+        viewModel.next()
+
+        // Then
+        XCTAssertEqual(viewModel.currentStep, .viewingTermOfUse)
+        XCTAssertTrue(viewModel.isCurrentInputValid)
     }
 }
--- SignUpSampleUITests/US1SignUp/US1S14TermOfUseUITests.swift
+++ SignUpSampleUITests/US1SignUp/US1S14TermOfUseUITests.swift
@@ -4,30 +4,45 @@
 //
 //  Created by Nam Tran on 12/5/21.
 //
+//
+//  US1 - User story 1 : Sign Up
+//
+//  As a guest user,
+//  I want to create my account
+//  so that I can login to the system.
+//
+//  Viewing term of use
+//
+//  US1-S14 - Scenario 14: Term of use.
+//
+//  Given I started the sign up wizard
+//  when I go to the last step
+//  then I am able to view the term of use
+//  and the submit button is enable.
+//
 
 import XCTest
 
-class US1S14TermOfUseUITests: XCTestCase {
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class US1S14TermOfUseUITests: US1SignUpXCTestCase {
+    func testScenario() throws {
+        // Given
+        firstNameField.typeText("Nam")
+        nextButton.tap()
+        emailField.typeText("robo8590@gmail.com")
+        nextButton.tap()
+        passwordField.typeText("Nam123!")
+        nextButton.tap()
+        websiteField.typeText("http://www.linkedin.com/in/robo8590")
+        nextButton.tap()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Then
+        XCTAssertTrue(titleLabel.exists)
+        XCTAssertFalse(instructionLabel.exists)
+        XCTAssertFalse(websiteLabel.exists)
+        XCTAssertFalse(websiteField.exists)
+        XCTAssertFalse(nextButton.exists)
+        XCTAssertTrue(termOfUseTitleLabel.exists)
+        XCTAssertTrue(termOfUseView.exists)
+        XCTAssertTrue(submitButton.exists)
     }
 }
